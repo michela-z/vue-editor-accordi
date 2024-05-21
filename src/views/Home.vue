858f75f24e9fcd6700875ab610e1c24e8b9ba0f7
Michela Zallocco <michelazallocco94@gmail.com>
2024-05-21 12:10:06 +0200

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,23 @@
         <div class="container">
             <Editor />
         </div>
+        <p class="info">Developed by <a href="https://michela-z.github.io/">Michela Zallocco</a></p>
     </div>
 </template>
 
 <script setup>
     import Editor from '../components/Editor.vue'
 </script>
+
+<style>
+    .info {
+        position: absolute;
+        bottom: 10px;
+        left: 50%;
+        transform: translateX(-50%)
+    }
+    a {
+        text-decoration: none;
+        color: #2e7eff;
+    }
+</style>
